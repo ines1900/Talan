e0b0ddef18f265d0ef9df59c25887fd3f16ededa
ines1900 <103032627+ines1900@users.noreply.github.com>
2022-04-14 22:30:05 +0100

    Update Act-1.8.2.java

--- Act-1.8/Act-1.8.2.java
+++ Act-1.8/Act-1.8.2.java
@@ -1,26 +1,49 @@
 package ProjetMusique;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
-import java.util.ArrayList;
+public class Lecteur {
 
-public class sauvegarde {
-	//lire les musiques
-	BufferReader reader = new BufferReader(new inputStreamReader(new FileInputStream());
-	
-	File source = new File ("C:/Users/Desktop/dossiermusique1/");
-	File dest = new File ("C:/Users/Desktop/dossiermusique2/" + source.getName)
-	ArrayList<MusiqueImpl> array = playListe.getArray();
-	string music = scanner  
-			try{
-	  files.copy(source.topath(), dest.topath());
-	}catch (IOexception e) {
-	   e.printStackTrace();
-	}	
-}
-	@Override
-	public int sauvetdeplacer(ArrayList<MusiqueImpl> array, String titre) {
-		return e1.getNom();
-}
-
-public class SauvegardeMusic {
-
+	public static void main(String[] args) {
+		File file= new File("Ma play list.doc");
+		 BufferedReader bufferreader = null;
+        try {
+			FileReader fileReader=new FileReader(file);
+			bufferreader = new BufferedReader(fileReader);
+		} catch (FileNotFoundException e) {
+			System.err.printf("le fichier %s n'a pas été trouvé",file.toString());
+			
+		}
+        try {
+		try {
+			bufferreader.close();
+		} catch (IOException e) {
+			System.err.printf("Impossible de fermer le fichier",file.toString());
+		}
+        } catch(NullPointerException e) {
+        	System.err.printf("Impossible d'ouvrir le fichier",file.toString());}
+	}
+	// Fonction enregistrement la playlist d'un utilisateur dans un autre.
+	public void Enregistrement() {
+		try {
+		File file1= new File("Ma play list.doc");
+		File file2= new File("User play list.doc");
+		FileWriter filewriter = new FileWriter("User play list.doc");
+		PrintWriter printWriter= new PrintWriter (filewriter );
+		Scanner scan = new Scanner(file1);
+		while (scan.hasNextLine()) {
+			String texte=scan.nextLine();
+			printWriter.println(texte);}
+		printWriter.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
